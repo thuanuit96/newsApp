9e090e9deb5b0faabf1b1ccd8087e4958ec4c768
thuanuit96 <thuanuit96@gmail.com>
2021-09-24 10:30:00 +0700

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by VN01-MAC-0006 on 24/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
